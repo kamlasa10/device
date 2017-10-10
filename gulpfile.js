5fca962c05dc3ae129a52b2cb970e9234a1a5e3f
kamlasa10 <kamlasa@ukr.net>
2017-10-10 22:59:59 +0300

    Сверстал header

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var sass        = require('gulp-sass');
+
+gulp.task('serve', ['sass'], function() {
+  browserSync.init({
+    server: "app"
+  });
+  gulp.watch("app/sass/**/*.scss", ['sass']);
+  gulp.watch("app/*.html").on('change', browserSync.reload);
+});
+gulp.task('sass', function() {
+  return gulp.src("app/sass/**/*.scss")
+      .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+      .pipe(gulp.dest("app/css"))
+      .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
